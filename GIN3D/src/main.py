11bc7da6cbeba83065893dde7110bd2b0e01226d
Senocak <senocak@HIPERSIMLAB09.boisestate.edu>
2017-05-24 11:29:08 -0600

    several bugs are fixed

--- GIN3D/src/main.py
+++ GIN3D/src/main.py
@@ -1,16 +1,16 @@
 import sys
 
 from GIN3DConfigWin import *
-
+#inanc
 if len(sys.argv) == 2:
     cfg_file = sys.argv[1]
 elif len(sys.argv) == 1:
     cfg_file = None
 else:
-    print 'Usage: program [.cfg file]'
+    print ('Usage: program [.cfg file]')
     sys.exit(1)
 
 if cfg_file is None or cfg_file.endswith('.cfg'):
     GIN3DConfigWin(cfg_file)
 else:
-    print 'Input file must have .cfg extension: ', cfg_file
+    print ('Input file must have .cfg extension: ', cfg_file)
